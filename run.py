c05794b8226ad4c8c12e0346a75487a540f7de4a
YuZz <diyuhuang5@gmail.com>
2026-06-25 00:02:52 +0800

    微調：加入雲端 Port 自動適應邏輯

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+import os
 from app.routes import app
 
 if __name__ == "__main__":
-    print("準備啟動 Flask 本地端伺服器...")
-    # 開啟 debug 模式，這樣你修改程式碼時伺服器會自動重新載入
-    app.run(debug=True, port=5000)
+    print("正在啟動 Flask 伺服器...")
+    
+    # 這裡很關鍵：雲端上有分配 Port 就用雲端的，沒有（像是本地端）就預設用 5000
+    port = int(os.environ.get("PORT", 5000))
+    
+    # host="0.0.0.0" 代表允許外網的所有人透過網址連線進來
+    app.run(host="0.0.0.0", port=port)
